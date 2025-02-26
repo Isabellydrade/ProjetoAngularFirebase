31a02b3163f79ddce5874e446e7b379b272ac827
Isabellydrade <141058433+Isabellydrade@users.noreply.github.com>
2025-02-26 17:39:44 +0000

    testee

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -12,17 +12,22 @@ import { Router } from '@angular/router';
 })
 export class HomePage {
 
-  pokemon:any = {
-    nome: null,
-    poder: null
-  };
+  url: string = `https://restcountries.com/v3.1/all?fields=name,flag`;
+  is_loading: boolean = false;// para fazer a bolinha de carregamento 
+  dados: any = null // esta any pq ela aceita qualquer coisa
 
-  constructor(
-    public crudService: CrudService
-   ){ }
-
-  enviar() {
-    this.crudService.insert(this.pokemon, 'pokemons');
-  }
-
+ constructor(){
+  fetch(this.url)// aqui coloca this porque e uma função de fora que esta chamando para ser executada 
+  .then(dados => dados.json())//converter os dados, "=> e como se fosse uma função anonima"
+  .then(dados => {
+    console.log(dados);
+    this.dados = dados; 
+  })//exibir os dados do then de cima 
+  .catch(_ => {
+    console.log(_);  "se colocar _ no lugar do erro exibe so para o dev "
+  })//tratar possiveis erros
+  .finally(() => {
+    console.log('Requisição Finalizada!');
+  })//exibir que finalizou 
+ }
 }
